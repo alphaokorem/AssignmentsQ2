37989f16cb075851eae55a2b1367345aaf1de1ca
Abdul Hadi Faisal <alphaokorem@gmail.com>
2024-09-22 22:20:47 +0500

    assignment2 done

---
+++ assign2/src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+export default function Header() {
+    return(
+        <>
+            <div id="text">
+            <h1>Abdul Hadi Faisal</h1>
+            <h2>Front-end Developer studying at GIAIC, exploring AI, MetaVerse and Blockchain</h2>
+            <h4>May look older, but I am 19 years old</h4>
+            </div>
+            <img src="/me.jpg" alt="my pic"/>
+        </>
+    )
+}

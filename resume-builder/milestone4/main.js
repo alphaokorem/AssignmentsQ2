ab7881645ebd6da7d70528487652ad979290dc5f
Abdul Hadi Faisal <alphaokorem@gmail.com>
2024-11-17 21:50:27 +0500

    editable resume

---
+++ resume-builder/milestone4/main.js
@@ -0,0 +1,43 @@
+var imageInput = document.getElementById("image");
+var userNameInput = document.getElementById("UserName");
+var jobTitleInput = document.getElementById("jobTitle");
+var aboutMeInput = document.getElementById("aboutme");
+var educationInput = document.getElementById("education");
+var skillInput = document.getElementById("skill1");
+var experienceInput = document.getElementById("experience");
+var contactPhoneInput = document.getElementById("contactPhone");
+var emailInput = document.getElementById("email");
+var linkedinInput = document.getElementById("field");
+if (linkedinInput.value) {
+    var link = document.getElementById("linkedin");
+    var field = document.getElementById("field");
+    field.style.display = "none"; //hide the input field
+    link.style.display = "none"; //hide the link input field
+}
+function generateResume() {
+    if (imageInput.files) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var _a;
+            var imgElement = document.createElement("img");
+            //creates a new img element
+            imgElement.src = (_a = e.target) === null || _a === void 0 ? void 0 : _a.result;
+            imgElement.style.width = "200px"; //set width and
+            imgElement.style.borderRadius = "10px"; //set border radius as was done in the original milestone1
+            imageInput.replaceWith(imgElement);
+            //replaces the file input field with the new image element
+        };
+        reader.readAsDataURL(imageInput.files[0]);
+    }
+    userNameInput.outerHTML = "<h1 contenteditable=\"true\">".concat(userNameInput.value, "</h1>");
+    jobTitleInput.outerHTML = "<p contenteditable=\"true\">".concat(jobTitleInput.value, "</p>");
+    aboutMeInput.outerHTML = "<p contenteditable=\"true\">".concat(aboutMeInput.value, "</p>");
+    educationInput.outerHTML = "<li contenteditable=\"true\">".concat(educationInput.value, "</li>");
+    skillInput.outerHTML = "<li contenteditable=\"true\">".concat(skillInput.value, "</li>");
+    experienceInput.outerHTML = "<li contenteditable=\"true\">".concat(experienceInput.value, "</li>");
+    contactPhoneInput.outerHTML = "<p contenteditable=\"true\">".concat(contactPhoneInput.value, "</p>");
+    emailInput.outerHTML = "<p contenteditable=\"true\">".concat(emailInput.value, "</p>");
+    linkedinInput.outerHTML = "<p contenteditable=\"true\">".concat(linkedinInput.value, "</p>");
+    var button = document.getElementById("generateButton");
+    button.style.display = "none";
+}
---
+++ resume-builder/milestone4/main.ts
@@ -0,0 +1,48 @@
+const imageInput = document.getElementById("image") as HTMLInputElement;
+const userNameInput = document.getElementById("UserName") as HTMLInputElement;
+const jobTitleInput = document.getElementById("jobTitle") as HTMLInputElement;
+const aboutMeInput = document.getElementById("aboutme") as HTMLTextAreaElement;
+const educationInput = document.getElementById("education") as HTMLTextAreaElement;
+const skillInput = document.getElementById("skill1") as HTMLTextAreaElement;
+const experienceInput = document.getElementById("experience") as HTMLTextAreaElement;
+const contactPhoneInput = document.getElementById("contactPhone") as HTMLInputElement;
+const emailInput = document.getElementById("email") as HTMLInputElement;
+const linkedinInput = document.getElementById("field") as HTMLInputElement;
+
+  if(linkedinInput.value) {
+    const link = document.getElementById("linkedin") as HTMLDivElement;
+    const field = document.getElementById("field") as HTMLDivElement;
+    field.style.display = "none"; //hide the input field
+    link.style.display = "none"; //hide the link input field
+  }
+
+function generateResume() {
+  if (imageInput.files) { 
+    const reader = new FileReader();   
+    reader.onload = (e) => { 
+      const imgElement = document.createElement("img"); 
+                            //creates a new img element
+      imgElement.src = e.target?.result as string; 
+      imgElement.style.width = "200px"; //set width and
+      imgElement.style.borderRadius = "10px"; //set border radius as was done in the original milestone1
+      imageInput.replaceWith(imgElement); 
+//replaces the file input field with the new image element
+    };
+    reader.readAsDataURL(imageInput.files[0]); 
+}
+  
+
+  userNameInput.outerHTML = `<h1 contenteditable="true">${userNameInput.value}</h1>`;
+  jobTitleInput.outerHTML = `<p contenteditable="true">${jobTitleInput.value}</p>`;
+  aboutMeInput.outerHTML = `<p contenteditable="true">${aboutMeInput.value}</p>`;
+  educationInput.outerHTML = `<li contenteditable="true">${educationInput.value}</li>`;
+  skillInput.outerHTML = `<li contenteditable="true">${skillInput.value}</li>`;
+  experienceInput.outerHTML = `<li contenteditable="true">${experienceInput.value}</li>`;
+  contactPhoneInput.outerHTML = `<p contenteditable="true">${contactPhoneInput.value}</p>`;
+  emailInput.outerHTML = `<p contenteditable="true">${emailInput.value}</p>`;
+  linkedinInput.outerHTML = `<p contenteditable="true">${linkedinInput.value}</p>`;
+
+
+  const button = document.getElementById("generateButton") as HTMLInputElement;
+  button.style.display = "none";
+}
